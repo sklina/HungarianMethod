88a920041c069b12109c628bc670dbf097b7f7e0
Alina Skorokhodova <alina.skorokhodova@vistar.su>
2016-12-03 17:25:51 +0300

    change classes ZerosPosition

--- src/hungarianmethod/HungarianMethod.java
+++ src/hungarianmethod/HungarianMethod.java
@@ -383,4 +383,48 @@ public class HungarianMethod {
 
         check += alpha * (matrix.getColumnDimension() - k);
     }
+	
+	public static class ZerosPosition {
+
+		private final int row;
+		private final int column;
+
+		public ZerosPosition(int row, int column) {
+			this.row = row;
+			this.column = column;
+		}
+
+		public int getRow() {
+			return row;
+		}
+
+		public int getColumn() {
+			return column;
+		}
+
+		@Override
+		public String toString() {
+			return "Zero[row=" + (row + 1)
+					+ ", column=" + (column + 1)
+					+ "]";
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (!(o instanceof ZerosPosition)) {
+				return false;
+			}
+			ZerosPosition pairo = (ZerosPosition) o;
+			return this.row == pairo.getRow()
+					|| this.column == pairo.getColumn();
+		}
+
+		@Override
+		public int hashCode() {
+			int hash = 7;
+			hash = 19 * hash + this.row;
+			hash = 19 * hash + this.column;
+			return hash;
+		}
+	}
 }
